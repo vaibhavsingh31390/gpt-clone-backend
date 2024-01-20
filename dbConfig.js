380fceb093a3b6b0915fd710ea8cb6dd348643ed
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2024-01-21 04:32:50 +0530

    [UPDATES] connection fix

--- dbConfig.js
+++ dbConfig.js
@@ -5,7 +5,6 @@ const username = process.env.DB_USERNAME;
 const userpass = process.env.DB_PASS;
 const host = process.env.DB_HOST;
 // const userpass = null;
-console.log(name, username, userpass, host);
 const sequelize = new Sequelize(name, username, userpass, {
   host: host,
   dialect: "mariadb",
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config({ path: "./config.env" });
 const app = require("./app");
 const port = process.env.APP_PORT;
-const sequelize = require("./dbConfig");
+const sequelize = require("./dbConfig.js");
 
 async function startApp() {
   try {
@@ -12,7 +12,10 @@ async function startApp() {
       console.log(`App running on ${port} in ${process.env.APP_ENV} mode.`);
     });
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error(
+      `Unable to connect to the database:   [${process.env.DB_NAME}, ${process.env.DB_USERNAME}, ${process.env.DB_PASS}], ${process.env.DB_HOST}`,
+      error
+    );
   }
 }
 
