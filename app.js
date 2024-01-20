76748b2e62ce3d7f27d37b3017b9cc60eede0038
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2024-01-21 04:54:07 +0530

    [UPDATE] added vercel endpint fix

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ require("dotenv").config({ path: "./config.env" });
 if (process.env.APP_ENV === "DEV") {
   app.use(morgan("dev"));
 }
+
+const allowedOrigins = ["https://chatgpt-cloned.netlify.app"];
 const corsOptions = {
-  origin: "https://chatgpt-cloned.netlify.app/",
-  credentials: true,
-  optionsSuccessStatus: 200,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Add this line for enabling credentials
+  optionsSuccessStatus: 200, // Add this line to set the options success status to 200
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
